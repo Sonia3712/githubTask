195c1bbad2744f65a2b66a77f540636ff11c4524
Sonia3712 <f223712@cfd.nu.edu.pk>
2024-11-15 10:12:58 +0500

    main added

---
+++ src/repo/main.java
@@ -0,0 +1,15 @@
+
+package repo;
+public class main 
+{
+   public static void main(String[] args) 
+   {
+      System.out.println("Welcome to the application!");
+      greetUser();
+    }
+
+   public static void greetUser() 
+   {
+       System.out.println("Hello, User!");
+    }
+}
